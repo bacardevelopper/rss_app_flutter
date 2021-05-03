b1c08ba5d61c05f5dbdaadb66e3cd0c6197688d2
kufanya-up <kufanya.agency@gmail.com>
2021-05-03 15:00:16 +0200

    enregistrement

--- lib/models/parser.dart
+++ lib/models/parser.dart
@@ -1,10 +1,18 @@
 import 'package:webfeed/webfeed.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:async';
 
 // class pour parser ---
 class Parser {
+  final urlMayotte = "https://la1ere.francetvinfo.fr/mayotte/actu/rss";
   Future chargerRss() async {
-    
+    final reponse = await http.get(Uri.parse(urlMayotte));
+    if(reponse.statusCode == 200){
+      print("Reussit : "+reponse.statusCode.toString());
+      return RssFeed.parse(reponse.body);
+    }else {
+      print("Error : "+reponse.statusCode.toString());
+      return RssFeed.parse(reponse.body);
+    }
   }
 }
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:rss_app_flutter/models/parser.dart'; // importation relative
-import 'dart:convert';
+import 'package:webfeed/webfeed.dart';
 // ------
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
@@ -10,28 +10,34 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
+
 // ------
 class _HomeState extends State<Home> {
   /* LES VARIABLES DE FLUX */
-
-
-  
+  var leTitre;
+  var laDesc;
+  var lien;
+  RssFeed feed;
+  List data;
   /* LES VARIABLES DE FLUX */
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     attenteResParse();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.yellowAccent,
       appBar: new AppBar(
-        backgroundColor: Colors.purpleAccent,
+        backgroundColor: Colors.black,
         title: new Text(
-          "App de flux RSS",
+          "Résumé info Mayotte 1ere",
           textAlign: TextAlign.center,
+          style: TextStyle(
+              fontStyle: FontStyle.italic, color: Colors.yellowAccent),
         ),
         centerTitle: true,
       ),
@@ -39,7 +45,26 @@ class _HomeState extends State<Home> {
   }
 
   // Fonction Future : ()
-  Future <void> attenteResParse () async {
-    
+  Future attenteResParse() async {
+    RssFeed recu = await Parser().chargerRss();
+    if (recu != null) {
+      setState(() {
+        feed = recu;
+        data = feed.items; /* (array de donnée = data[0].title) */
+        leTitre = data[0].title;
+        laDesc = data[0].description;
+        lien = data[0].link;
+        print(feed.items.length);
+
+        data.forEach((element) {
+          print("element");
+          print("titre : "+element.title);
+          print("description : "+element.description.toString());
+          print("lien "+element.link);
+        });
+      });
+    }
   }
+
+
 }
